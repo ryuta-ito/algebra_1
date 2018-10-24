1b6de1d090ca703daab2b7f70d767406c53a9531
ryuta-ito <ir47839@gmail.com>
2018-10-24 21:53:41 +0900

    general map relation condition

--- relation.v
+++ relation.v
@@ -15,6 +15,17 @@ Definition transitive {X : Type} (R : relation X) :=
     R x y -> R y z -> R x z.
 Arguments transitive {X} R /.
 
+Definition gen_rel (X Y : Type) := X -> Y -> Prop.
+
+Definition map_rel {X Y : Type} (R : gen_rel X Y) :=
+  forall x, forall y y', R x y -> R x y' -> y = y'.
+  (* forall x x', x = x' -> forall y y', R x y -> R x' y' -> y = y'. *)
+Arguments map_rel {X Y} R/.
+
+Definition inverse_rel {X Y : Type} (R : gen_rel X Y) (R' : gen_rel Y X) :=
+  forall x y, R x y -> R' y x.
+Arguments inverse_rel {X Y} R R'/.
+
 Definition eq_relation {X : Type} (R : relation X) :=
   reflexive R /\ symmetric R /\ transitive R.
 Arguments eq_relation {X} R /.
@@ -29,6 +40,13 @@ Structure eq_R X := {
 Definition eq_class {X : Type} (r : eq_R X) x : set X :=
   fun y => eq_r X r x y.
 Arguments eq_class {X} r x/.
+
+Theorem x_belongs_x_eq_class : forall {X : Type} (r : eq_R X) x,
+  belongs x (eq_class r x).
+Proof.
+  simpl. intros.
+  apply (refl X r).
+Qed.
 
 Theorem belongs_y_z_in_eq_class__y_z_is_eq :
   forall {X : Type} (x y z : X) (r : eq_R X),
